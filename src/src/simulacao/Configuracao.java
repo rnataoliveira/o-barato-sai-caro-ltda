723f93a51972b81fe597a2c27325282c2f0c454c
Renata Oliveira <renatabels@gmail.com>
2016-10-20 01:28:34 -0200

    ++

--- src/src/simulacao/Configuracao.java
+++ src/src/simulacao/Configuracao.java
@@ -2,20 +2,34 @@ package simulacao;
 
 import mercado.Caixa;
 
+/*
+ * Representa uma configuração de atendimento do mercado para um determinado horario
+ * com uma determinada quantidade de caixas operacionais
+ * */
 public class Configuracao {
-	
+
 	private Horario _horario;
 	private Iterable<Caixa> _caixasAtivos;
 	
+	/*
+	 * @param horario O horario a ser simulado
+	 * @param caixasAtivos Lista de caixas ativos para a simulação desta configuração
+	 * */
 	public Configuracao(Horario horario, Iterable<Caixa> caixasAtivos) {
 		_horario = horario;
 		_caixasAtivos = caixasAtivos;
 	}
 	
+	/*
+	 * @return Lista de caixas ativos para esta configuração
+	 * */
 	public Iterable<Caixa> caixasAtivos() {
 		return _caixasAtivos;
 	} 
 	
+	/*
+	 * @return horario para esta configuração
+	 * */
 	public Horario horario() {
 		return _horario;
 	}
--- src/src/simulacao/Horario.java
+++ src/src/simulacao/Horario.java
@@ -1,22 +1,20 @@
 package simulacao;
 
-import java.sql.Time;
-
 public class Horario {
 	
-	private Time _de;
-	private Time _ate;
+	private int _de;
+	private int _ate;
 	
-	public Horario(Time de, Time ate) {
+	public Horario(int de, int ate) {
 		_de = de;
 		_ate = ate;
 	}
 	
-	public Time de(){
+	public int de(){
 		return _de;
 	}
 	
-	public Time ate(){
+	public int ate(){
 		return _ate;
 	}
 }
