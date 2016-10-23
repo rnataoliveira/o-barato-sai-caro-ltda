99cca08333d67373d4d09f536ed3c9d0fdf9c770
Renata Oliveira <renatabels@gmail.com>
2016-10-23 13:05:51 -0200

    add +

--- src/src/mercado/Caixa.java
+++ src/src/mercado/Caixa.java
@@ -1,5 +1,16 @@
 package mercado;
 
 public class Caixa {
+	double valorFaturado;
+	private static final double CUSTO_OPERACIONAL = 300;
 	
+	public int atender(Pessoa pessoa){
+		int tempoAtendimento = 0;
+		for(Item i : pessoa.compra()){
+			valorFaturado += i.preco();
+			tempoAtendimento += 1;
+		}
+		double valorFaturadoFinal = valorFaturado - CUSTO_OPERACIONAL;
+		return tempoAtendimento;
+	}
 }
