f56f8450e81c5ab321e17e61c569226d9925c856
Renata Oliveira <renatabels@gmail.com>
2016-11-16 02:49:27 -0200

    att

--- src/src/simulacao/Simulador.java
+++ src/src/simulacao/Simulador.java
@@ -1,8 +1,6 @@
 package simulacao;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
